ad4f925d8450bdc8ac54763684e24efc40a2d404
Andrey Donskoy <donskoi.com@gmail.com>
2023-04-13 16:48:33 +0300

    add hw 4

---
+++ 04/custom_meta.py
@@ -0,0 +1,43 @@
+from typing import Any
+
+
+class CustomMeta(type):
+
+    def setter(cls, name: str, val: Any):
+        name = f"custom_{name}"
+        cls.__dict__[name] = val
+
+    def __init__(cls, name, bases, classdict: dict, **kwargs):
+        super().__init__(name, bases, classdict, **kwargs)
+
+    def __call__(cls, *args, **kwargs):
+        return super().__call__(*args, **kwargs)
+
+    def __new__(mcs, name, bases, classdict: dict, **kwargs):
+
+        new_classdict = dict()
+        new_classdict["__setattr__"] = mcs.setter
+
+        for key, value in classdict.items():
+
+            if key[:2] != "__" and key[-2:] != "__":
+                new_classdict["custom_" + key] = value
+            else:
+                new_classdict[key] = value
+
+        cls = super().__new__(mcs, name, bases, new_classdict, **kwargs)
+
+        return cls
+
+
+class CustomClass(metaclass=CustomMeta):
+    x = 50
+
+    def __init__(self, val: int = 99):
+        self.val = val
+
+    def line(self):
+        return 100
+
+    def __str__(self):
+        return "Custom_by_metaclass"
---
+++ 04/custom_meta_tests.py
@@ -0,0 +1,54 @@
+from custom_meta import CustomMeta, CustomClass
+import unittest
+
+
+class TestCustomMetaClass(unittest.TestCase):
+
+    def test_call(self):
+        class TestClass(metaclass=CustomMeta):
+            pass
+
+        cls = TestClass()
+        self.assertIsInstance(cls, TestClass)
+
+    def test_new(self):
+        class TestClass(metaclass=CustomMeta):
+            new_attribute = 1703
+
+        self.assertEqual(TestClass.custom_new_attribute, 1703)
+
+
+class TestCustomClass(unittest.TestCase):
+    def setUp(self) -> None:
+        self.custom_class = CustomClass()
+        self.another_custom_class = CustomClass(val=900)
+
+    def test_default_init(self):
+        self.assertEqual(self.custom_class.custom_val, 99)
+
+    def test_init(self):
+        self.assertEqual(self.another_custom_class.custom_val, 900)
+
+    def test_line(self):
+        self.assertEqual(self.custom_class.custom_line(), 100)
+        self.assertEqual(self.another_custom_class.custom_line(), 100)
+
+    def test_str(self):
+        self.assertEqual(str(self.custom_class), "Custom_by_metaclass")
+        self.assertEqual(str(self.another_custom_class), "Custom_by_metaclass")
+
+    def test_new_attributes(self):
+        self.custom_class.another_attribute = 123
+        self.custom_class.another_attribute_2 = None
+
+        self.assertEqual(self.custom_class.custom_another_attribute, 123)
+        self.assertEqual(self.custom_class.custom_another_attribute_2, None)
+
+        self.assertEqual(
+            {
+                "custom_val": 99,
+                "custom_another_attribute": 123,
+                "custom_another_attribute_2": None,
+            },
+            self.custom_class.__dict__
+        )
