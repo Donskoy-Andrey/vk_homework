55ea90fdbad71bf11fe54ac49ea08656afc356d5
Andrey Donskoy <donskoi.com@gmail.com>
2023-05-10 20:02:40 +0300

    add valid\invalid tests

--- 04/descriptors_tests.py
+++ 04/descriptors_tests.py
@@ -55,3 +55,35 @@ class TestDescriptor(unittest.TestCase):
         self.assertIsNone(cls.num)
         self.assertIsNone(cls.name)
         self.assertIsNone(cls.price)
+
+    def test_change_to_valid(self):
+        try:
+            cls = Data(num=3, name="yoghurt", price=12)
+            cls.num = 5
+            cls.name = "bread"
+            cls.price = 100
+        except ValueError:
+            self.fail("Value error. Incorrect arguments.")
+
+        self.assertEqual(cls.num, 5)
+        self.assertEqual(cls.name, "bread")
+        self.assertEqual(cls.price, 100)
+
+    def test_change_to_invalid(self):
+        cls = Data(num=3, name="yoghurt", price=12)
+
+        with self.assertRaises(ValueError) as error:
+            cls.num = "1"
+        self.assertEqual(error.exception.args, ("The value is not an integer.",))
+
+        with self.assertRaises(ValueError) as error:
+            cls.name = 1001
+        self.assertEqual(error.exception.args, ("The value is not a string.",))
+
+        with self.assertRaises(ValueError) as error:
+            cls.price = -100
+        self.assertEqual(error.exception.args, ("The value is not a positive integer.",))
+
+        self.assertEqual(cls.num, 3)
+        self.assertEqual(cls.name, "yoghurt")
+        self.assertEqual(cls.price, 12)
